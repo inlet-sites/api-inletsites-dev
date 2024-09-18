0bbf8e8408cce01dc5eb20a7bf76db0f01ab7a98
Lee Morgan <lee@leemorgan.dev>
2024-09-18 11:38:07 -0400

    Add mongoose and cors. Set up both in the entry file.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 import express from "express";
 import compression from "compression";
+import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
+let mongoString = "mongodb://127.0.0.1/inletsites";
+if(process.env.NODE_ENV === "production"){
+    mongoString = `mongodb://website:${process.env.MONGODB_PASS}@127.0.0.1:27017/inletsites?authSource=admin`;
+}
+mongoose.connect(mongoString);
+
 app.use(compression());
+app.use(express.json());
+app.use(cors());
 
 import userRoutes from "./routes/user.js";
 userRoutes(app);
