3c0d6d6abcbfaf495088ecde3a14b9f77084caee
Lee Morgan <lee@leemorgan.dev>
2024-11-21 17:14:11 -0500

    Create album model.

---
+++ models/album.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const AlbumSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    photos: [{
+        file: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: false
+        },
+        created: {
+            type: Date,
+            required: true
+        }
+    }],
+    created: {
+        type: Date,
+        required: true
+    }
+});
+
+export default mongoose.model("album", AlbumSchema);
